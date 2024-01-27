dd7fb024e6d7b7f815fcda0f908714f1a71208f4
Patrik Hartlen <patrik.hartlen@se.com>
2024-01-27 15:34:47 +0100

    No many bar if running in tty

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -11,7 +11,10 @@
              '("melpa" . "http://melpa.org/packages/"))
 
 ;; ---------------------------------------------------------- [ style settings ]
- (use-package color-theme-sanityinc-tomorrow
+(unless (display-graphic-p)
+  (menu-bar-mode -1))
+
+(use-package color-theme-sanityinc-tomorrow
   :ensure t
   :config
   (load-theme 'sanityinc-tomorrow-blue t))
@@ -99,7 +102,7 @@
  '(custom-safe-themes
    '("76ddb2e196c6ba8f380c23d169cf2c8f561fd2013ad54b987c516d3cabc00216" default))
  '(package-selected-packages
-   '(smartparens rainbow-delimiters nerd-icons highlight-indent-guides dirvish color-theme-sanityinc-tomorrow color-theme ace-jump-mode)))
+   '(golden-ratio smartparens rainbow-delimiters nerd-icons highlight-indent-guides dirvish color-theme-sanityinc-tomorrow color-theme ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
