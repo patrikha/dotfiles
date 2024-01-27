95a065234856abbc5878f6298b81deabf25bae42
Patrik Hartlen <patrik.hartlen@se.com>
2024-01-27 18:38:42 +0100

    Fixed spelling

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -102,7 +102,7 @@
  '(custom-safe-themes
    '("76ddb2e196c6ba8f380c23d169cf2c8f561fd2013ad54b987c516d3cabc00216" default))
  '(package-selected-packages
-   '(golden-ratio smartparens rainbow-delimiters nerd-icons highlight-indent-guides dirvish color-theme-sanityinc-tomorrow color-theme ace-jump-mode)))
+   '(find-file-in-repository find-file-in-project golden-ratio smartparens rainbow-delimiters nerd-icons highlight-indent-guides dirvish color-theme-sanityinc-tomorrow color-theme ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- dot_emacs.d/lisp/init-ivy.el
+++ dot_emacs.d/lisp/init-ivy.el
@@ -27,7 +27,7 @@
 ;; (setq search-default-mode #'char-fold-to-regexp)
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-x f") 'find-file-in-repository)
-(Global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
