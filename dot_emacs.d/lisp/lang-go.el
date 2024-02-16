7cbd5fdea6bfda3eaf38aa375fe1cdae2c4d0cf8
Patrik Hartlen <patrik.hartlen@se.com>
2024-02-16 11:10:55 +0100

    Add missing golang file

---
+++ dot_emacs.d/lisp/lang-go.el
@@ -0,0 +1,10 @@
+;; lang-go.el --- Rust language mode
+;;; Commentary:
+;;; Created by Patrik Hartlén
+
+;; pacman -S gopls
+;; yay tree-sitter-go tree-sitter-gomod-git
+
+(use-package go-ts-mode)
+
+(provide 'lang-go)
