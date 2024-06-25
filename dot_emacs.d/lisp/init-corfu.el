7da2918d2a9f054857d1e131f54560537e1e440c
Patrik Hartlén <patrik.hartlen@se.com>
2024-06-25 08:02:21 +0200

    Added corfu

---
+++ dot_emacs.d/lisp/init-corfu.el
@@ -0,0 +1,38 @@
+(use-package corfu
+  :ensure t
+  ;; TAB-and-Go customizations
+  :custom
+  (corfu-cycle t)           ;; Enable cycling for `corfu-next/previous'
+  (corfu-preselect 'prompt) ;; Always preselect the prompt
+  (tab-always-indent 'complete)
+  ;; Use TAB for cycling, default is `corfu-complete'.
+  :bind
+  (:map corfu-map
+        ("TAB" . corfu-next)
+        ([tab] . corfu-next)
+        ("S-TAB" . corfu-previous)
+        ([backtab] . corfu-previous))
+  :hook (corfu-mode . corfu-popupinfo-mode)
+  :init
+  (global-corfu-mode))
+
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :init
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+;;(add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)
+
+;; Optionally:
+(setq nerd-icons-corfu-mapping
+      '((array :style "cod" :icon "symbol_array" :face font-lock-type-face)
+        (boolean :style "cod" :icon "symbol_boolean" :face font-lock-builtin-face)
+        ;; ...
+        (t :style "cod" :icon "code" :face font-lock-warning-face)))
+        ;; Remember to add an entry for `t', the library uses that as default.
+
+;; The Custom interface is also supported for tuning the variable above.
+
+
+(provide 'init-corfu)
