4bd0d77d899f1729c2ccf01a5ba6c76c6e675d28
Patrik Hartlen <patrik.hartlen@se.com>
2024-11-28 18:16:37 +0100

    Added some default lsp servers

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -11,7 +9,11 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
-        -- add more arguments for adding more language servers
+        "jsonls",
+        "yamlls",
+        "pyright",
+        "rust_analyzer",
+        "typst_lsp",
       },
     },
   },
