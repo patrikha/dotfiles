21335e51fe6f9fa6af4dc33427cb4dc21649c9ff
Patrik Hartlen <patrik.hartlen@se.com>
2024-11-30 21:50:21 +0100

    Added better whitespace

---
+++ dot_config/nvim/lua/plugins/vim-better-whitespace.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "ntpeters/vim-better-whitespace",
+    config = function()
+      vim.g.better_whitespace_enabled = 1
+      vim.g.strip_whitespace_on_save = 1
+      vim.g.strip_whitespace_confirm = 0
+      vim.g.show_spaces_that_precede_tabs = 1
+    end,
+    lazy = true,
+  },
+}
